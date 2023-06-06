651add0b54570e82e61185ead24322a9091a2803
victorgrosu <vagrosu@yahoo.com>
2023-06-07 00:46:11 +0300

    init project

---
+++ src/test/java/com/gourmetGo/GourmetGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourmetGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
